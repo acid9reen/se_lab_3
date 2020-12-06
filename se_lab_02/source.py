b4d6fb1921b032209f26a5b379dddfed70b3e334
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-12-06 14:38:56 +0300

    wtf

--- se_lab_02/source.py
+++ se_lab_02/source.py
@@ -2,6 +2,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+plt.style.use('dark_background')
+P = 0.6
+
 class Integrator:
     def __init__(self, matrix: np.matrix, step: float) -> None:
 
@@ -28,8 +31,17 @@ class Integrator:
         return {"x": x_next, "y": y_vec_next}
 
 
+def is_green(curr_point, prev_point):
+    return curr_point - prev_point > 0
+
+
 def plot(**series):
-    plt.plot(*series.values())
+    # plt.plot(*series.values())
+    plt.plot(*series.values(), 'r')
+
+    plt.xlabel('y_1')
+    plt.ylabel('y_2')
+    plt.title(f"p = {P}")
     plt.show()
 
 
@@ -38,7 +50,11 @@ def main():
 
     step = 0.001
     x_min = 0
-    x_max = np.pi + 0.015
+    x_max = 50
+    eps = 0.01
+    match_counter = 0
+    prev_green = False
+    p = P
     y_0 = np.matrix([[0, 1]], dtype=np.longdouble)
 
     integrator = Integrator(A, step)
@@ -48,6 +64,8 @@ def main():
     x_curr = x_min
     y_curr = y_0
 
+    point_to_connect = y_0.item((0, 0))
+
     while x_curr < x_max:
         point = integrator.next_point(x_curr, y_curr)
 
@@ -56,8 +74,20 @@ def main():
         y_1 = np.append(y_1, point["y"].item((0, 0)))
         y_2 = np.append(y_2, point["y"].item((0, 1)))
 
-    y_1 = np.append(y_1, y_1[0])
-    y_2 = np.append(y_2, y_2[0])
+        if abs(y_1[-1] - point_to_connect) < eps:
+            try:
+                prev_point = y_1[-2]
+            except:
+                prev_point = y_1[-1] + 2 * eps
+
+            if prev_green and y_1[-1] - prev_point < 0:
+                y_curr = np.matrix([y_1[-1], y_2[-1] + p], dtype=np.longdouble)
+                prev_green = False
+
+            prev_green = is_green(y_1[-1], prev_point)
+
+    # y_1 = np.append(y_1, y_1[0])
+    # y_2 = np.append(y_2, y_2[0])
 
     plot(f=y_1, y=y_2)
 
